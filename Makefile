607ef7ce781b932ba0b4c1cc470721b5e9579a7b
gabrielmancini <gabriel.mancini@gmail.com>
2012-04-29 21:29:47 -0300

    add styling and fix the make file

--- Makefile
+++ Makefile
@@ -2,7 +2,9 @@
 RELEASE = alpha
 
 JS_TARGETS = \
+	lib/pouchdb/pouch.alpha.js \
     src/puton.js 
 
 all :
+	$(MAKE) -C  lib/pouchdb/
 	(echo "(function() { "; cat $(JS_TARGETS); echo " })(this);") > puton.$(RELEASE).js
--- src/puton.js
+++ src/puton.js
@@ -67,7 +67,7 @@
 		var self = this;
 		
 		this.tpl.loadTemplates(['changes_tpl', 'config_section_tpl', 'config_top_tpl', 'confirm_tpl', 'couchapps_tpl', 'create_doc_tpl', 'database_tpl', 'database_view_tpl', 'databases_tpl', 'document_tpl', 'edit_key_tpl', 'home_tpl', 'logged_in', 'logged_in_btn', 'logged_out', 'logged_out_btn', 'replication_doc_tpl', 'replication_items', 'replication_tpl', 'tasks_tpl', 'unauthorized_tpl'], function() {
-		  //  console.log(self.tpl.templates);
+		    console.log(self.tpl.templates);
 		});
 	
 	};
